6e15549171482857dcf15b2e7553d90af492008c
AndreaCodone <105943523+AndreaCodone@users.noreply.github.com>
2023-06-21 13:39:16 +0200

    Esercitazione 20-06-2023

---
+++ 20-06-2023/src/mocks/posts.js
@@ -0,0 +1,122 @@
+export const postsData = [
+  {
+    id: 1,
+    userId: 1,
+    userImage: "https://robohash.org/hicveldicta.png?size=50x50&set=set1",
+    userName: "joshua_l",
+    location: "Tokio, Japan",
+    isLike: true,
+    media: [
+      {
+        id: "media000001djdjdjd",
+        type: "image",
+        src: "https://picsum.photos/id/1/375/375",
+      },
+    ],
+    comments: [
+      {
+        id: "comm393939393",
+        userId: 222999,
+        userName: "josua_l",
+        content: "Il nostro commento",
+      },
+    ],
+    likes: {
+      numberOfLikes: 44686,
+      featuredLike: {
+        userId: 2929292,
+        username: "craig_love",
+      },
+    },
+  },
+  {
+    id: 2,
+    userId: 2,
+    userImage: "https://robohash.org/doloremquesintcorrupti.png",
+    userName: "atuny0",
+    location: "Palermo, Sicily",
+    isLike: false,
+    media: [
+      {
+        id: "media000001djdjdjd",
+        type: "image",
+        src: "https://picsum.photos/id/2/375/375",
+      },
+    ],
+    comments: [
+      {
+        id: "comm393939393",
+        userId: 222999,
+        userName: "josua_l",
+        content: "Il nostro commento",
+      },
+    ],
+    likes: {
+      numberOfLikes: 44686,
+      featuredLike: {
+        userId: 2929292,
+        username: "craig_love",
+      },
+    },
+  },
+  {
+    id: 3,
+    userId: 3,
+    userImage: "https://robohash.org/hicveldicta.png?size=50x50&set=set1",
+    userName: "joshua_l",
+    location: "Tokio, Japan",
+    isLike: false,
+    media: [
+      {
+        id: "media000001djdjdjd",
+        type: "image",
+        src: "https://picsum.photos/id/3/375/375",
+      },
+    ],
+    comments: [
+      {
+        id: "comm393939393",
+        userId: 222999,
+        userName: "josua_l",
+        content: "Il nostro commento",
+      },
+    ],
+    likes: {
+      numberOfLikes: 44686,
+      featuredLike: {
+        userId: 2929292,
+        username: "craig_love",
+      },
+    },
+  },
+  {
+    id: 4,
+    userId: 4,
+    userImage: "https://robohash.org/doloremquesintcorrupti.png",
+    userName: "atuny0",
+    location: "Palermo, Sicily",
+    isLike: false,
+    media: [
+      {
+        id: "media000001djdjdjd",
+        type: "image",
+        src: "https://picsum.photos/id/4/375/375",
+      },
+    ],
+    comments: [
+      {
+        id: "comm393939393",
+        userId: 222999,
+        userName: "josua_l",
+        content: "Il nostro commento",
+      },
+    ],
+    likes: {
+      numberOfLikes: 56432,
+      featuredLike: {
+        userId: 2929292,
+        username: "craig_love",
+      },
+    },
+  },
+];
